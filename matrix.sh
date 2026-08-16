b2599ff2fbdc0209b01ff6101bb56f086d6bacdd
renner <renner0@posteo.de>
2026-08-16 20:48:43 +0200

    try

--- matrix.sh
+++ matrix.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-# IMAGES='["aurora", "aurora-dx"]'
+# IMAGS='["aurora", "aurora-dx"]'
 # IMAGE_FLAVORS='["main", "nvidia-open"]'
-# STREAM_NAME='["testing"]'
+# STREAM_NAME="testing"
 # ARCHITECTURES='["amd64", "arm64"]'
 #
-# IMAGES='["aurora", "aurora-dx"]' IMAGE_FLAVORS='["main", "nvidia-open"]' STREAM_NAME='["testing"]' ARCHITECTURES='["amd64", "arm64"]' matrix.sh
+# IMAGES='["aurora", "aurora-dx"]' IMAGE_FLAVORS='["main", "nvidia-open"]' STREAM_NAME="testing" ARCHITECTURES='["amd64", "arm64"]' matrix.sh
+
+# This is for github actions to dynamically generate the following images
+# aurora aurora-dx aurora-nvidia-open aurora-dx-nvidia-open
+# They each consist of 2 images, arm64 and amd64, each group is independent from the others
+
 
 jq -cn \
   --argjson base_names "$IMAGES" \
@@ -19,6 +24,7 @@ jq -cn \
       $base_names[] as $b |
       $image_flavors[] as $f |
       {
+        group: (if $f == "main" then $b else "\($b)-\($f)" end),
         image_name: $b,
         image_flavor: $f,
         stream_name: $stream_name,
