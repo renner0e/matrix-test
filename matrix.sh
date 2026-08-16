d1082c406febbdb062a07a56b81e1815ba22a49b
renner <renner0@posteo.de>
2026-08-16 17:57:58 +0200

    no json for stream

--- matrix.sh
+++ matrix.sh
@@ -11,19 +11,18 @@ set -euox pipefail
 jq -cn \
   --argjson base_names "$IMAGES" \
   --argjson image_flavors "$IMAGE_FLAVORS" \
-  --argjson stream_name "$(jq -R . <<< "$STREAM_NAME" || echo "$STREAM_NAME")" \
+  --arg stream_name "$STREAM_NAME" \
   --argjson architectures "$ARCHITECTURES" \
   '
   {
     include: [
       $base_names[] as $b |
       $image_flavors[] as $f |
-      (if ($stream_name | type) == "array" then $stream_name[] else $stream_name end) as $s |
       $architectures[] as $a |
       {
         image_name: $b,
         image_flavor: $f,
-        stream_name: $s,
+        stream_name: $stream_name,
         architecture: $a
       }
     ]
